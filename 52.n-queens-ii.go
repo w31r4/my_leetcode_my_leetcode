dca78fc14d6bc9b3b6d7f426efad8251bc0dab96
zenfun <zenfun510@gmail.com>
2025-08-27 01:26:55 +0800

    52 solved plus

--- 52.n-queens-ii.go
+++ 52.n-queens-ii.go
@@ -10,68 +10,70 @@ package leetcode
 
 // @lcpr-template-end
 // @lc code=start
-func operateQueen(i, j, n int, board [][]int, isPut bool) {
-	num := 0
-	if isPut {
-		num = i + 1
-	}
-	for index := 0; index < n; index++ {
-		if board[i][index] == 0 || board[i][index] == i+1 {
-			board[i][index] = num
-		}
-	}
-	for index := 0; index < n; index++ {
-		if board[index][j] == 0 || board[index][j] == i+1 {
-			board[index][j] = num
-		}
-	}
-	index1 := i
-	index2 := j
-	for index1 < n && index2 < n {
-		if board[index1][index2] == 0 || board[index1][index2] == i+1 {
-			board[index1][index2] = num
-		}
-		index1++
-		index2++
-	}
-	index1 = i
-	index2 = j
-	for index1 >= 0 && index2 >= 0 {
-		if board[index1][index2] == 0 || board[index1][index2] == i+1 {
-			board[index1][index2] = num
-		}
-		index1--
-		index2--
-	}
-	index1 = i
-	index2 = j
-	for index1 >= 0 && index2 < n {
-		if board[index1][index2] == 0 || board[index1][index2] == i+1 {
-			board[index1][index2] = num
-		}
-		index1--
-		index2++
-	}
-	index1 = i
-	index2 = j
-	for index1 < n && index2 >= 0 {
-		if board[index1][index2] == 0 || board[index1][index2] == i+1 {
-			board[index1][index2] = num
-		}
-		index1++
-		index2--
-	}
+// func operateQueen(i, j, n int, board [][]int, isPut bool) {
+// 	num := 0
+// 	if isPut {
+// 		num = i + 1
+// 	}
+// 	for index := 0; index < n; index++ {
+// 		if board[i][index] == 0 || board[i][index] == i+1 {
+// 			board[i][index] = num
+// 		}
+// 	}
+// 	for index := 0; index < n; index++ {
+// 		if board[index][j] == 0 || board[index][j] == i+1 {
+// 			board[index][j] = num
+// 		}
+// 	}
+// 	index1 := i
+// 	index2 := j
+// 	for index1 < n && index2 < n {
+// 		if board[index1][index2] == 0 || board[index1][index2] == i+1 {
+// 			board[index1][index2] = num
+// 		}
+// 		index1++
+// 		index2++
+// 	}
+// 	index1 = i
+// 	index2 = j
+// 	for index1 >= 0 && index2 >= 0 {
+// 		if board[index1][index2] == 0 || board[index1][index2] == i+1 {
+// 			board[index1][index2] = num
+// 		}
+// 		index1--
+// 		index2--
+// 	}
+// 	index1 = i
+// 	index2 = j
+// 	for index1 >= 0 && index2 < n {
+// 		if board[index1][index2] == 0 || board[index1][index2] == i+1 {
+// 			board[index1][index2] = num
+// 		}
+// 		index1--
+// 		index2++
+// 	}
+// 	index1 = i
+// 	index2 = j
+// 	for index1 < n && index2 >= 0 {
+// 		if board[index1][index2] == 0 || board[index1][index2] == i+1 {
+// 			board[index1][index2] = num
+// 		}
+// 		index1++
+// 		index2--
+// 	}
 
-}
+// }
 
 func totalNQueens(n int) (answer int) {
 	//N 皇后的回溯
 	//建立一个棋盘，当放上一个皇后后，皇后及其可以攻击到的区域都赋值为 1
-
-	board := make([][]int, n)
-	for i := 0; i < n; i++ {
-		board[i] = make([]int, n)
-	}
+	columns := make([]int, n)
+	// board := make([][]int, n)
+	// for i := 0; i < n; i++ {
+	// 	board[i] = make([]int, n)
+	// }
+	mainDiag := make([]bool, 2*n-1)
+	sideDiag := make([]bool, 2*n-1)
 
 	var dfs func(level int)
 	dfs = func(level int) {
@@ -86,12 +88,18 @@ func totalNQueens(n int) (answer int) {
 		//存在浅拷贝的情况
 		for i := 0; i < n; i++ {
 			//第 level 行，第 i 列
-			if board[level][i] == 0 {
+			if columns[i] == 0 && !mainDiag[level-i+n-1] && !sideDiag[level+i] {
 				// copy(temp, board)
-				operateQueen(level, i, n, board, true)
+				// operateQueen(level, i, n, board, true)
+				columns[i] = 1
+				mainDiag[level-i+n-1] = true
+				sideDiag[i+level] = true
 				dfs(level + 1)
+				columns[i] = 0
+				mainDiag[level-i+n-1] = false
+				sideDiag[i+level] = false
 				// copy(board, temp)
-				operateQueen(level, i, n, board, false)
+				// operateQueen(level, i, n, board, false)
 			}
 		}
 		return
