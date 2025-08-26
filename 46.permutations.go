a228133802fc48c4900a99cd9fd762b1a21a5b10
zenfun <zenfun510@gmail.com>
2025-08-26 18:20:52 +0800

    46 solved plus

---
+++ 46.permutations.go
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=golang
+ * @lcpr version=30204
+ *
+ * [46] 全排列
+ */
+
+// @lcpr-template-start
+package leetcode
+
+// @lcpr-template-end
+// @lc code=start
+func getCap(n int) int {
+	cap := 1
+	for i := 1; i <= n; i++ {
+		cap *= i
+	}
+	return cap
+}
+func permute(nums []int) (answer [][]int) {
+	length := len(nums)
+	cap := getCap(length)
+	//阶乘分配空间量
+	answer = make([][]int, 0, cap)
+	//提前分配好空间
+	path := make([]int, length)
+	visited := make([]bool, length)
+	var dfs func(level int)
+	dfs = func(level int) {
+		if level == length {
+			temp := make([]int, length)
+			copy(temp, path)
+			answer = append(answer, temp)
+			return
+		}
+		for i, v := range nums {
+			if !visited[i] {
+				path[level] = v
+				visited[i] = true
+				dfs(level + 1)
+				visited[i] = false
+			}
+		}
+	}
+	dfs(0)
+	return
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+*/
