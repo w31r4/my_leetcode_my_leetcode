fd77dc03b921194c9c7d31c709c8c7c71a97220d
zenfun <zenfun510@gmail.com>
2025-08-27 19:42:50 +0800

    79 solved plus

---
+++ 79.word-search.go
@@ -0,0 +1,151 @@
+/*
+ * @lc app=leetcode.cn id=79 lang=golang
+ * @lcpr version=30204
+ *
+ * [79] 单词搜索
+ */
+
+// @lcpr-template-start
+package leetcode
+
+// @lcpr-template-end
+// @lc code=start
+// 获取棋盘中所有字母出现的次数
+// 由于字母包含大小写，所以有 52 个
+func getBoardLetterTimes(board [][]byte) [52]int {
+	var letterMap [52]int // Our array is now size 52
+
+	for _, line := range board {
+		for _, ch := range line {
+			if ch >= 'a' && ch <= 'z' {
+				// It's a lowercase letter, map to 0-25
+				letterMap[ch-'a']++
+			} else if ch >= 'A' && ch <= 'Z' {
+				// It's an uppercase letter, map to 26-51
+				letterMap[ch-'A'+26]++
+			}
+		}
+	}
+	return letterMap
+}
+
+// 获取单词中所有字母出现的次数
+// 由于字母包含大小写，所以有 52 个
+func getWordLetterTimes(word string) (wordLetterCounts [52]int) {
+	for _, ch := range word {
+		if ch >= 'a' && ch <= 'z' {
+			// It's a lowercase letter, map to 0-25
+			wordLetterCounts[ch-'a']++
+		} else if ch >= 'A' && ch <= 'Z' {
+			// It's an uppercase letter, map to 26-51
+			wordLetterCounts[ch-'A'+26]++
+		}
+	}
+	return wordLetterCounts
+}
+
+// 通过 rune 切片将 string 反转
+func reverseStringRunes(s string) string {
+	// 1. 将 string 转换为 []rune
+	runes := []rune(s)
+
+	// 2. 使用双指针法交换元素
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	// 3. 将 []rune 转换回 string
+	return string(runes)
+}
+func exist(board [][]byte, word string) (answer bool) {
+	boardCounts := getBoardLetterTimes(board)
+	wordCounts := getWordLetterTimes(word)
+	for i, v := range boardCounts {
+		if wordCounts[i] > v {
+			return
+		}
+	}
+	var firstLetterNum, lastLetterNum int
+	length := len(word)
+
+	if word[0] >= 'a' && word[0] <= 'z' {
+		// It's a lowercase letter, map to 0-25
+		firstLetterNum = int(word[0] - 'a')
+	} else if word[0] >= 'A' && word[0] <= 'Z' {
+		// It's an uppercase letter, map to 26-51
+		firstLetterNum = int(word[0]-'A') + 26
+	}
+
+	if word[length-1] >= 'a' && word[length-1] <= 'z' {
+		// It's a lowercase letter, map to length-1-25
+		lastLetterNum = int(word[length-1] - 'a')
+	} else if word[length-1] >= 'A' && word[length-1] <= 'Z' {
+		// It's an uppercase letter, map to 26-51
+		lastLetterNum = int(word[length-1]-'A') + 26
+	}
+	if boardCounts[firstLetterNum] > boardCounts[lastLetterNum] {
+		word = reverseStringRunes(word)
+	}
+	visited := make([][]bool, len(board))
+	m := len(board)
+	n := len(board[0])
+	for i, _ := range visited {
+		visited[i] = make([]bool, len(board[0]))
+	}
+	var dfs func(i, j, level int)
+	dfs = func(i, j, level int) {
+
+		if answer {
+			return
+		}
+		if level == length {
+			answer = true
+			return
+		}
+		if i < 0 || j < 0 || i == m || j == n {
+			return
+		}
+		if visited[i][j] {
+			return
+		}
+		if board[i][j] != word[level] {
+			return
+		}
+
+		visited[i][j] = true
+		dfs(i+1, j, level+1)
+		dfs(i, j+1, level+1)
+		dfs(i-1, j, level+1)
+		dfs(i, j-1, level+1)
+		visited[i][j] = false
+		return
+	}
+	//使用了外部变量 answer 进行剪枝，会导致代码高度耦合
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if !answer {
+				dfs(i, j, 0)
+			} else {
+				break
+			}
+		}
+	}
+	return
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']]\n"ABCCED"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']]\n"SEE"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']]\n"ABCB"\n
+// @lcpr case=end
+
+*/
