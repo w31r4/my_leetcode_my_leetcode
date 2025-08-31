75a6c2565fd3157bd0fec7baacb8633c45bf2ab0
zenfun <zenfun510@gmail.com>
2025-09-01 00:47:22 +0800

    34 solved by myself plus

--- 34.find-first-and-last-position-of-element-in-sorted-array.go
+++ 34.find-first-and-last-position-of-element-in-sorted-array.go
@@ -6,11 +6,59 @@
  */
 
 // @lcpr-template-start
+package leetcode
 
 // @lcpr-template-end
 // @lc code=start
 func searchRange(nums []int, target int) []int {
-
+	// 首先找到位置，然后左右开找边界
+	//左闭右开
+	answer := []int{-1, -1}
+	length := len(nums)
+	right := length
+	left := 0
+	isFound := false
+	mid := 0
+	for left < right {
+		mid = left + (right-left)/2
+		if nums[mid] == target {
+			isFound = true
+			break
+		}
+		if target > nums[mid] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	if !isFound {
+		return answer
+	}
+	midNum := mid
+	left = midNum
+	right = length
+	for left < right {
+		mid = left + (right-left)/2
+		if target == nums[mid] {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	answer[1] = left - 1
+	right = midNum - 1
+	left = -1
+	for left < right {
+		mid = left + (right-left)/2 + 1
+		//加一确保重心后置，让 (1+2)/2=2
+		if target == nums[mid] {
+			right = mid - 1
+		} else {
+			left = mid
+		}
+	}
+	answer[0] = left + 1
+	return answer
 }
 
 // @lc code=end
@@ -29,4 +77,3 @@ func searchRange(nums []int, target int) []int {
 // @lcpr case=end
 
 */
-
